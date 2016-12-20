397cb8d2982876dbba9f65ba0be75b7e8683daa5
reid holmes <rtholmes@gmail.com>
2016-12-19 17:10:51 -0800

    initial push of the bootstrap project. lots of commands work directly but aren't working with the npm run aliases. need to figure that out.

---
+++ src/controller/InsightFacade.ts
@@ -0,0 +1,25 @@
+/**
+ * This is the main programmatic entrypoint for your system.
+ */
+import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
+
+import Log from "../Util";
+
+export default class InsightFacade implements IInsightFacade {
+
+    constructor() {
+        Log.trace('InsightFacadeImpl::init()');
+    }
+
+    addDataset(id: string, content: string): Promise<InsightResponse> {
+        return null;
+    }
+
+    removeDataset(id: string): Promise<InsightResponse> {
+        return null;
+    }
+
+    performQuery(query: QueryRequest): Promise <InsightResponse> {
+        return null;
+    }
+}
---
+++ test/EchoSpec.ts
@@ -0,0 +1,40 @@
+/**
+ * Created by rtholmes on 2016-10-31.
+ */
+
+import Server from "../src/rest/Server";
+import {expect} from 'chai';
+import Log from "../src/Util";
+
+describe("EchoSpec", function () {
+
+
+    it("Should be able to echo", function () {
+        let out = Server.performEcho('echo');
+        Log.test(JSON.stringify(out));
+        expect(out.code).to.equal(200);
+        expect(out.body).to.deep.equal({message: 'echo...echo'});
+    });
+
+    it("Should be able to echo silence", function () {
+        let out = Server.performEcho('');
+        Log.test(JSON.stringify(out));
+        expect(out.code).to.equal(200);
+        expect(out.body).to.deep.equal({message: '...'});
+    });
+
+    it("Should be able to handle a missing echo message sensibly", function () {
+        let out = Server.performEcho(undefined);
+        Log.test(JSON.stringify(out));
+        expect(out.code).to.equal(400);
+        expect(out.body).to.deep.equal({error: 'Message not provided'});
+    });
+
+    it("Should be able to handle a null echo message sensibly", function () {
+        let out = Server.performEcho(null);
+        Log.test(JSON.stringify(out));
+        expect(out.code).to.equal(400);
+        expect(out.body).to.deep.equal({error: 'Message not provided'});
+    });
+
+});
